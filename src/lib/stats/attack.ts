0b2d7f0e2703f5f330e723bfc40a3d8e91356fae
TheBeav01 <adbeaver17@yahoo.com>
2025-05-10 15:17:38 -0400

    Fixed 1 damage issue

--- src/lib/stats/attack.ts
+++ src/lib/stats/attack.ts
@@ -32,7 +32,7 @@ export class AttackUtils {
         // Big pos diff = small attack. Small diff = attack does ~ x hp. Big neg diff = more damage
         const attackDefenseDifferential = (attack / defense).toFixed(2)
         
-        let finalAttack = Math.min(1, attack * Number.parseFloat(attackDefenseDifferential))
+        let finalAttack = Math.max(1, attack * Number.parseFloat(attackDefenseDifferential))
         return Math.round(finalAttack)
     }
 }
